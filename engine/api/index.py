e27e0148befa08da783a513f53e9c14793c98378
Shankar Bhagat <shankar.bhagat@siliconstack.com.au>
2026-07-22 10:08:18 +0530

    restructure

--- engine/api/index.py
+++ engine/api/index.py
@@ -1,14 +1,1 @@
-import sys
-import os
-
-# Ensure the engine directory and root directory are in Python sys.path
-current_dir = os.path.dirname(os.path.abspath(__file__))
-parent_dir = os.path.abspath(os.path.join(current_dir, ".."))
-engine_dir = os.path.join(parent_dir, "engine")
-
-if engine_dir not in sys.path:
-    sys.path.insert(0, engine_dir)
-if parent_dir not in sys.path:
-    sys.path.insert(0, parent_dir)
-
 from main import app
